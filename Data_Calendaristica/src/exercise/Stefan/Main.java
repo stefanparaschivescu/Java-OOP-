6c11a108a31883df64eaf5fdb202c43e33774e66
stefanparaschivescu <67150958+stefanparaschivescu@users.noreply.github.com>
2020-06-19 15:44:03 +0300

    Add files via upload

---
+++ Data_Calendaristica/src/exercise/Stefan/Main.java
@@ -0,0 +1,11 @@
+package exercise.Stefan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        DataCalendaristica data = new DataCalendaristica(25,12,2019);
+
+
+    }
+}
